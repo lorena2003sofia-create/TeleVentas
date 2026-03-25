581e134f331816af0ec1a0657f2f5aa3530ccc38
lorena2003sofia-create <lorena2003sofia@gmail.com>
2026-03-24 20:02:31 -0500

    Se agrega clase OrdenCompra

---
+++ orden.py
@@ -0,0 +1,18 @@
+class OrdenCompra:
+
+    def __init__(self, cliente):
+        self.cliente = cliente
+        self.productos = []
+        self.estado = "Activa"
+
+    def agregar_producto(self, producto):
+        self.productos.append(producto)
+
+    def mostrar_orden(self):
+        print("Orden de:", self.cliente.nombre)
+        for p in self.productos:
+            print("-", p.nombre)
+
+    def cancelar(self):
+        self.estado = "Cancelada"
+        print("Orden cancelada")
