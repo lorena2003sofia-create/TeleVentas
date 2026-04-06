556de6f29de7d4226c505b1cd1b57b4b34aebfa9
lorena2003sofia-create <lorena2003sofia@gmail.com>
2026-04-05 23:31:48 -0500

    Create cliente.py

---
+++ TeleVentas/TeleVentas/cliente.py
@@ -0,0 +1,10 @@
+class Cliente:
+
+    def __init__(self,nombre,correo):
+
+        self.nombre=nombre
+        self.correo=correo
+
+    def hacer_queja(self,mensaje):
+
+        print("Queja enviada:",mensaje)
