63b30bb89050cb3e2dd4d3b9418561c4b1a5833b
lorena2003sofia-create <lorena2003sofia@gmail.com>
2026-04-05 23:29:27 -0500

    Create producto.py

---
+++ TeleVentas/producto.py
@@ -0,0 +1,10 @@
+class Producto:
+
+    def __init__(self, codigo, nombre, precio, cantidad):
+        self.codigo = codigo
+        self.nombre = nombre
+        self.precio = precio
+        self.cantidad = cantidad
+
+    def mostrar_info(self):
+        print(self.codigo, self.nombre, "$", self.precio, "Stock:", self.cantidad)
